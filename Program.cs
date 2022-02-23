6c80a070e70565732d0a7c73cfff21642c478ccb
Salaudeen Fatai <olamilekanpheity@gmail.com>
2022-02-24 07:35:34 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Fatai {
+    class Program {
+        static void Main(string[] args) {
+            int i = 4;
+            double d = 4.0;
+            string s = "Polyilaro ";
+
+            Console.Write("Enter a second integer: ");
+            int sec_int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a second double: ");
+            double sec_double = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a second string: ");
+            string sec_str = Console.ReadLine();
+
+            Console.WriteLine(i + sec_int);
+            Console.WriteLine(d + sec_double);
+            Console.WriteLine(s + sec_str);
+
+        }
+    }
+}
